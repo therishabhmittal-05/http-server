192fd74ebb8d060d9d124d39e9d42b9f2eeb2f7a
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-04-04 23:05:10 +0530

    Okh Nothing is done

--- http-server-5/routes/ride1.js
+++ http-server-5/routes/ride1.js
@@ -1,22 +1,22 @@
-const express   =  require('express');
-const router = express.Router();
-function isEnoughAge(age){
-    if (age>14){
-        return true
-    }
-    else {
-        return false
-    }
-}
-router.get("/", (req, res)=>{
-    if(isEnoughAge(req.query.age)){
-        res.json({
-            message: "You are eligible for ride 1"
-        })
-    }
-    res.json({
-        message: "Sorry you are not eligible for ride 1"
-    })
-})
+// const express   =  require('express');
+// const router = express.Router();
+// function isEnoughAge(age){
+//     if (age>14){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// router.get("/", (req, res)=>{
+//     if(isEnoughAge(req.query.age)){
+//         res.json({
+//             message: "You are eligible for ride 1"
+//         })
+//     }
+//     res.json({
+//         message: "Sorry you are not eligible for ride 1"
+//     })
+// })
 
-module.exports =  router
+// module.exports =  router
--- http-server-5/servermain.js
+++ http-server-5/servermain.js
@@ -8,9 +8,28 @@ function isEnoughAge(age){
         return false
     }
 }
+let reqCount = 0;
+function reqCounter(req, res, next){
+    reqCount++;
+    console.log(reqCount)
+    next()
 
-const ride1 = require("./routes/ride1")
-app.use('/ride1', ride1)
+}
 
+// const ride1 = require("./routes/ride1")
+// app.use('/ride1', ride1)
+
+
+app.get("/ride1", reqCounter, (req, res)=>{
+    if(isEnoughAge(req.query.age)){
+        res.json({
+            message: "You are eligible for ride 1"
+        })
+    }
+    res.json({
+        message: "Sorry you are not eligible for ride 1"
+    })
+})
+// console.log(reqCount)
 
 app.listen(3000)
