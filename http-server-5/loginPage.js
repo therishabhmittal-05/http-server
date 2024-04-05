ce3d1975db27ed8c51dc0040c941ea0fd7a506e2
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-04-05 12:17:53 +0530

    Login page

---
+++ http-server-5/loginPage.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();  
+const jwt = require('jsonwebtoken')
+const jwtPassword = 'secret'
+app.use(express.json())
+
+const allUsers = [{username: 'admin', password: 'admin'}, {username: 'user', password: 'user'}]
+
+function isUser(username, password){
+    return allUsers.find(function(user){
+        return user.username === username && user.password === password;
+    })
+}
+
+
+app.post('/login', (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    if(!isUser(username, password)){
+        res.status(401).send('Invalid username or password')
+        return;
+    }
+    const token = jwt.sign({username: username}, jwtPassword);
+    return res.json({
+        token
+    })
+})
+
+app.get('/users', (req, res)=>{
+    const token = req.headers.authorization
+    try{
+        const decoded = jwt.verify(token, jwtPassword)
+        const username = decoded.username
+        return res.json(allUsers.find((user)=>user.username !== username))
+    }
+    catch(err){
+        return res.json({error: 'Invalid token'})
+    }
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000')
+})
